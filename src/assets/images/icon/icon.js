4e6843e016f7670d6b19ec4254793681c4b5eeab
atikur-24 <atikurrahman7223@gmail.com>
2024-03-24 12:30:22 +0600

    fixed admin svg

--- src/assets/images/icon/icon.js
+++ src/assets/images/icon/icon.js
@@ -1,5 +1,7 @@
-import adminIcon from "@/assets/images/icon/admin.svg";
+import admin from "@/assets/images/icon/admin.svg";
 import longArrowIcon from "@/assets/images/icon/long-arrow.png";
 import videoIcon from "@/assets/images/icon/video.svg";
+
+const adminIcon = admin;
 
 export { adminIcon, longArrowIcon, videoIcon };
