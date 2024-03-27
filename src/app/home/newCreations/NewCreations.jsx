23e738eceb9d8b13e433496c15c3a76794570787
atikur-24 <atikurrahman7223@gmail.com>
2024-03-27 17:39:35 +0600

    fixed new creations section

--- src/app/home/newCreations/NewCreations.jsx
+++ src/app/home/newCreations/NewCreations.jsx
@@ -1,37 +1,28 @@
 "use client";
 
-import { bakilda } from "@/assets/Fonts/font";
 import Container from "@/components/ui/Container";
-import MyBtn from "@/components/ui/MyBtn";
-import { creationImages } from "@/data";
+import SectionTitle from "@/components/ui/SectionTitle";
+import { creationData } from "@/data";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { useState } from "react";
 
 const NewCreations = () => {
-  const [toggleState, setToggleState] = useState(1);
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
-
-  const Burger = creationImages?.filter((item) => item.category === "Burger");
-  const Drinks = creationImages?.filter((item) => item.category === "Drinks");
-  const Pizza = creationImages?.filter((item) => item.category === "Pizza");
-  const Dinner = creationImages?.filter((item) => item.category === "Dinner");
-  const Cookies = creationImages?.filter((item) => item.category === "Cookies");
-  const Bakery = creationImages?.filter((item) => item.category === "Bakery");
-
+  const [toggleTab, setToggleTab] = useState(1);
   const [index, setIndex] = useState(-1);
   const [displayCount, setDisplayCount] = useState(5);
 
-  const currentImage = creationImages[index];
-  const nextIndex = (index + 1) % creationImages.length;
-  const nextImage = creationImages[nextIndex] || currentImage;
-  const prevIndex = (index + creationImages.length - 1) % creationImages.length;
-  const prevImage = creationImages[prevIndex] || currentImage;
+  const burger = creationData?.filter((item) => item.category === "Burger");
+  const drinks = creationData?.filter((item) => item.category === "Drinks");
+  const pizza = creationData?.filter((item) => item.category === "Pizza");
+  const dinner = creationData?.filter((item) => item.category === "Dinner");
+  const lunch = creationData?.filter((item) => item.category === "Lunch");
+  const cookies = creationData?.filter((item) => item.category === "Cookies");
+  const bakery = creationData?.filter((item) => item.category === "Bakery");
 
-  // Calculation container width based on the number of creationImages and gap
-  // const containerWidth = creationImages.length * (424 + 24) - 24;
+  const handleToggleTab = (index) => {
+    setToggleTab(index);
+  };
 
   const showMore = (filteredItems) => {
     setDisplayCount(filteredItems.length);
@@ -41,37 +32,34 @@ const NewCreations = () => {
     setDisplayCount(5);
   };
 
-  const renderFilteredServices = (filteredItems) => {
-    if (filteredItems && filteredItems.length > 0) {
+  const renderFilteredItem = (filteredItems) => {
+    if (filteredItems.length > 0) {
       return (
-        <div>
-          <div className="flex flex-wrap gap-[20px] ">
-            {filteredItems?.slice(0, displayCount).map((e, i) => {
-              return (
-                <div
-                  key={i}
-                  style={{
-                    width: e.width + "px",
-                    height: e.height + "px",
-                    borderRadius: e.radius + "px",
-                    overflow: "hidden",
-                    marginBottom: "24px",
-                  }}
-                  className="relative"
-                >
-                  <Image
-                    src={e.src}
-                    alt={e.caption}
-                    className="h-full w-full cursor-pointer object-cover"
-                  />
-                  <p className="absolute bottom-5 left-5 cursor-pointer rounded-[7px] border border-white border-opacity-50 px-2.5 py-0.5 text-[14px] text-white shadow-md hover:bg-[#86371C]">
-                    {e?.caption}
-                  </p>
-                </div>
-              );
-            })}
+        <div className="flex flex-wrap gap-[20px] ">
+          {filteredItems?.slice(0, displayCount).map((item) => {
+            return (
+              <div
+                key={item.id}
+                style={{
+                  width: item.width + "px",
+                  height: item.height + "px",
+                }}
+                className="relative mb-6 overflow-hidden rounded-3xl"
+              >
+                <Image
+                  src={item.img}
+                  alt={item.caption}
+                  placeholder="blur"
+                  className="h-full w-full object-cover"
+                />
+                <span className="absolute bottom-5 left-5 cursor-pointer rounded-sm border border-white border-opacity-50 px-2.5 py-0.5 text-xs text-white shadow-md transition-colors hover:bg-my-secondary md:rounded-md md:text-sm">
+                  {item?.caption}
+                </span>
+              </div>
+            );
+          })}
 
-            {filteredItems.length > 5 && (
+          {/* {filteredItems.length > 5 && (
               <div className="mx-auto mt-4 flex justify-center pb-[90px]">
                 {displayCount === filteredItems.length ? (
                   <MyBtn
@@ -89,210 +77,92 @@ const NewCreations = () => {
                   />
                 )}
               </div>
-            )}
-
-            {/* {!!currentImage && (
-              <Lightbox
-                mainSrc={currentImage.original}
-                imageTitle={currentImage.caption}
-                mainSrcThumbnail={currentImage.src}
-                nextSrc={nextImage.original}
-                nextSrcThumbnail={nextImage.src}
-                prevSrc={prevImage.original}
-                prevSrcThumbnail={prevImage.src}
-                onCloseRequest={handleClose}
-                onMovePrevRequest={handleMovePrev}
-                onMoveNextRequest={handleMoveNext}
-              />
             )} */}
-          </div>
         </div>
       );
     } else {
       return (
-        <p className="my-20 flex flex-col items-center justify-center gap-4 text-xl text-red-500">
-          No products available for the selected rating or search.
+        <p className="my-10 flex flex-col items-center justify-center gap-4 text-base uppercase text-red-500 md:my-20 lg:text-xl">
+          Item not available on this tab
         </p>
       );
     }
   };
 
   return (
-    <Container className="mx-auto mt-[360px] lg:mt-[90px]">
-      <p className="text-center font-['Lemonada'] uppercase text-[#86371C]">
-        Showcase
-      </p>
-      <div className="mx-auto mt-2 h-[2px] w-[124px] bg-gradient-to-l from-[#df745151] via-[#86371C] to-[#df745151]"></div>
-
-      <div className="flex items-center justify-center">
-        <p
-          className={`${bakilda.className} mt-2.5 text-[22px] leading-[30px] lg:mt-3 lg:text-4xl xl:text-[44px] xl:leading-[60px]`}
-        >
-          Our Chefs New Creations
-        </p>
-      </div>
-      <p className="mx-auto mt-[19px] w-[340px] text-center text-[16px] leading-[20px] text-[#031811B2] text-opacity-70 lg:mt-[24px] lg:w-[674px] lg:text-[18px] lg:leading-[32px]">
+    <Container className="mt-[60px] lg:mt-[90px]">
+      <SectionTitle
+        title="Our Chefs New Creations"
+        subTitle="Showcase"
+        className="flex flex-col items-center justify-center"
+        leftStyle="hidden"
+        rightStyle="hidden"
+        bottomStyle={true}
+      />
+      <p className="mx-auto mt-[19px] text-[#031811] text-opacity-70 lg:mt-[24px] lg:w-[600px] lg:text-center">
         Behold the extraordinary creations born from the synergy of our
         users&apos; ingenuity and the transformative power of AI, a testament to
         boundless innovation.
       </p>
-
-      <div className="mx-auto mt-[60px]">
-        <div className="hidden items-center justify-center gap-5 md:mx-auto md:flex lg:mx-0 lg:flex">
-          <p
-            className={cn(
-              "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md",
-              toggleState === 1 &&
-                "cursor-pointer rounded-[50px] border-[1.5px] border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md",
-            )}
-            onClick={() => toggleTab(1)}
-          >
-            All
-          </p>
-          <p
-            className={
-              toggleState == 2
-                ? " cursor-pointer rounded-[50px] border-[1.5px] border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(2)}
-          >
-            Burger
-          </p>
-          <p
-            className={
-              toggleState == 3
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(3)}
-          >
-            Drinks
-          </p>
-          <p
-            className={
-              toggleState == 4
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(4)}
-          >
-            Pizza
-          </p>
-          <p
-            className={
-              toggleState == 5
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(5)}
-          >
-            Dinner
-          </p>
-          <p
-            className={
-              toggleState == 6
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(6)}
-          >
-            Cookies
-          </p>
-          <p
-            className={
-              toggleState == 7
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[24px] py-[12px] text-[22px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[24px] py-[12px] text-[22px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(7)}
-          >
-            Bakery
-          </p>
+      <div className="mt-6 flex items-center justify-center gap-5 md:mt-8 xl:mt-14">
+        <div
+          className={toggleTab === 1 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(1)}
+        >
+          All
         </div>
-
-        {/* for mobile  */}
-        <div className="mx-auto flex w-[340px] gap-x-[12px] overflow-x-auto lg:hidden">
-          <p
-            className={
-              toggleState == 1
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(1)}
-          >
-            All
-          </p>
-          <p
-            className={
-              toggleState == 2
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(2)}
-          >
-            Burger
-          </p>
-          <p
-            className={
-              toggleState == 3
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(3)}
-          >
-            Drinks
-          </p>
-          <p
-            className={
-              toggleState == 4
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(4)}
-          >
-            Pizza
-          </p>
-          <p
-            className={
-              toggleState == 5
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(5)}
-          >
-            Dinner
-          </p>
-          <p
-            className={
-              toggleState == 6
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(6)}
-          >
-            Cookies
-          </p>
-          <p
-            className={
-              toggleState == 7
-                ? " cursor-pointer rounded-[50px] border-[1.5px]  border-[#86371C] px-[10px] py-[5px] text-[16px] font-normal text-[#86371C] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-                : "cursor-pointer rounded-[50px] px-[10px] py-[5px] text-[16px] font-normal text-[#031811] shadow-[#86371C] hover:bg-[#86371C] hover:text-white hover:shadow-md"
-            }
-            onClick={() => toggleTab(7)}
-          >
-            Bakery
-          </p>
+        <div
+          className={toggleTab === 2 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(2)}
+        >
+          Burger
         </div>
-        <div className="mt-[48px]">
-          {toggleState === 1 && renderFilteredServices(creationImages)}
-          {toggleState === 2 && renderFilteredServices(Burger)}
-          {toggleState === 3 && renderFilteredServices(Drinks)}
-          {toggleState === 4 && renderFilteredServices(Pizza)}
-          {toggleState === 5 && renderFilteredServices(Dinner)}
-          {toggleState === 6 && renderFilteredServices(Cookies)}
-          {toggleState === 7 && renderFilteredServices(Bakery)}
+        <div
+          className={toggleTab === 3 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(3)}
+        >
+          Drinks
         </div>
+        <div
+          className={toggleTab === 4 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(4)}
+        >
+          Pizza
+        </div>
+        <div
+          className={toggleTab === 5 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(5)}
+        >
+          Dinner
+        </div>
+        <div
+          className={toggleTab === 6 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(6)}
+        >
+          Lunch
+        </div>
+        <div
+          className={toggleTab === 7 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(7)}
+        >
+          Cookies
+        </div>
+        <div
+          className={toggleTab === 8 ? "activeTab" : "defaultTab"}
+          onClick={() => handleToggleTab(8)}
+        >
+          Bakery
+        </div>
+      </div>
+      <div className="mt-8 md:mt-10 xl:mt-12">
+        {toggleTab === 1 && renderFilteredItem(creationData)}
+        {toggleTab === 2 && renderFilteredItem(burger)}
+        {toggleTab === 3 && renderFilteredItem(drinks)}
+        {toggleTab === 4 && renderFilteredItem(pizza)}
+        {toggleTab === 5 && renderFilteredItem(dinner)}
+        {toggleTab === 6 && renderFilteredItem(lunch)}
+        {toggleTab === 7 && renderFilteredItem(cookies)}
+        {toggleTab === 8 && renderFilteredItem(bakery)}
       </div>
     </Container>
   );
--- src/components/shared/Coming.jsx
+++ src/components/shared/Coming.jsx
@@ -1,6 +1,6 @@
 const Coming = () => {
   return (
-    <div className="lg:my-30 my-10 xl:my-32">
+    <div className="lg:my-30 my-10 text-center xl:my-32">
       <h3 className="text-xl font-semibold tracking-wide md:text-2xl lg:text-4xl lg:font-bold lg:tracking-wider">
         Coming Soon.....
       </h3>
