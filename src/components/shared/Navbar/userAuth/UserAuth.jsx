ceab6554e6446b87659daf366e41348a77886619
atikur-24 <atikurrahman7223@gmail.com>
2024-04-03 23:54:06 +0600

    implement a modal when user click sign in button

--- src/components/shared/Navbar/userAuth/UserAuth.jsx
+++ src/components/shared/Navbar/userAuth/UserAuth.jsx
@@ -1,6 +1,18 @@
+"use client";
 import { adminIcon } from "@/assets/images";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
-import Link from "next/link";
 
 const UserAuth = () => {
   return (
@@ -8,9 +20,46 @@ const UserAuth = () => {
       <div className="h-[25px] w-[25px] lg:h-[30px] lg:w-[30px]">
         <Image src={adminIcon} alt="admin" />
       </div>
-      <Link href="/sign-in" className="text-nowrap">
-        Sign in
-      </Link>
+      <Dialog>
+        <DialogTrigger>Sign in</DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="text-center font-medium text-black lg:font-semibold">
+              Login Your Account
+            </DialogTitle>
+            {/* <DialogDescription>
+              Make changes to your profile here. Click save when youre done.
+            </DialogDescription> */}
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="">
+                Email
+              </Label>
+              <Input
+                id="name"
+                defaultValue="Pedro Duarte"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="">
+                Password
+              </Label>
+              <Input
+                id="username"
+                defaultValue="@peduarte"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button variant="outline" type="submit">
+              Save changes
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,27 +1,26 @@
-import { Slot } from "@radix-ui/react-slot";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
-import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default:
-          "bg-secondary text-white leading-none text-xs md:text-sm lg:text-base",
-        outline:
-          "border border-secondary text-secondary bg-background text-xs md:text-sm lg:text-base hover:bg-secondary hover:text-white",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-6 lg:h-12 px-6 lg:px-10 py-4 lg:py-4 lg:rounded-lg",
+        default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
@@ -31,21 +30,18 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
-);
+  }
+)
 
-const Button = React.forwardRef(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
-Button.displayName = "Button";
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />)
+  );
+})
+Button.displayName = "Button"
 
-export { Button, buttonVariants };
+export { Button, buttonVariants }
