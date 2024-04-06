1b57ef2de9dd93a1f76e3e5e9eb4fa01fb092224
atikur-24 <atikurrahman7223@gmail.com>
2024-04-06 11:14:40 +0600

    added Readme.md file

--- README.md
+++ README.md
@@ -1,36 +1,18 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Coffee Shop
 
-## Getting Started
+## [Coffee Shop Live Link](https://coffee-shop-2-48697.web.app/)
 
-First, run the development server:
+## Project Features
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- Provide users with options to customize their orders, including size, strength, milk preference, and additional flavors.
+- Enable users to add products and manage their shopping carts for future purchases. Integrate a secure payment gateway (Ongoing).
+- Ensure the platform is responsive across various devices, including desktops, tablets, and smartphones.
+- Implement a search functionality by user name.
+- In this application fully responsive for mobile, tablet and desktop devices.
+- Some Features are Ongoing.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Used Technologies
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+- The website was built for client-side Next.js TailwindCSS, Shadcn ui.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### [Server Code](https://github.com/atikur-24/unlocklive-coffee-server)
--- src/components/shared/Navbar/userAuth/UserAuth.jsx
+++ src/components/shared/Navbar/userAuth/UserAuth.jsx
@@ -4,17 +4,40 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const UserAuth = () => {
+  const form = useForm(); // Initialize the form object
+
+  const onSubmit = (data) => {
+    // Handle form submission here
+  };
+
   return (
     <div className="inline-flex items-center gap-1 md:gap-2 lg:gap-3 xl:gap-x-[15px]">
       <div className="h-[25px] w-[25px] lg:h-[30px] lg:w-[30px]">
@@ -22,7 +45,7 @@ const UserAuth = () => {
       </div>
       <Dialog>
         <DialogTrigger>Sign in</DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="sm:max-w-[400px]">
           <DialogHeader>
             <DialogTitle className="text-center font-medium text-black lg:font-semibold">
               Login Your Account
@@ -32,27 +55,40 @@ const UserAuth = () => {
             </DialogDescription> */}
           </DialogHeader>
           <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
+            <div className="">
               <Label htmlFor="name" className="">
                 Email
               </Label>
-              <Input
-                id="name"
-                defaultValue="Pedro Duarte"
-                className="col-span-3"
-              />
+              <Input id="name" defaultValue="Pedro Duarte" className="" />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="username" className="">
                 Password
               </Label>
-              <Input
-                id="username"
-                defaultValue="@peduarte"
-                className="col-span-3"
-              />
+              <Input id="username" className="col-span-3" />
             </div>
           </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
           <DialogFooter>
             <Button variant="outline" type="submit">
               Save changes
