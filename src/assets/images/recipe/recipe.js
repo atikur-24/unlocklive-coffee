c164c7a08a4f9b4127d286886b2b031c215af12c
atikur-24 <atikurrahman7223@gmail.com>
2024-04-08 17:30:09 +0600

    recipe images are optimize

--- src/assets/images/recipe/recipe.js
+++ src/assets/images/recipe/recipe.js
@@ -1,7 +1,7 @@
-import recipeOne from "@/assets/images/recipe/recipe-1.png";
-import recipeTwo from "@/assets/images/recipe/recipe-2.png";
-import recipeThree from "@/assets/images/recipe/recipe-3.png";
-import recipeFour from "@/assets/images/recipe/recipe-4.jpg";
-import recipeFive from "@/assets/images/recipe/recipe-5.jpg";
+import recipeOne from "@/assets/images/recipe/recipe-1.webp";
+import recipeTwo from "@/assets/images/recipe/recipe-2.webp";
+import recipeThree from "@/assets/images/recipe/recipe-3.webp";
+import recipeFour from "@/assets/images/recipe/recipe-4.webp";
+import recipeFive from "@/assets/images/recipe/recipe-5.webp";
 
 export { recipeFive, recipeFour, recipeOne, recipeThree, recipeTwo };
